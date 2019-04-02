f76dd882a4c6858ce9ed52dd1094ca9d6e8cf34b
Kevin Tran Huu <kevin.tranhuu@gmail.com>
2019-04-02 12:22:13 -0400

    Implemented creating new games

---
+++ src/app/services/games.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Game } from '../models/game';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GamesService {
+
+  constructor(private http:HttpClient) { }
+
+  gameUrl: string = "http://localhost:8084/games";
+
+  createGame(game: Game): Observable<Game> {
+    return this.http.post<Game>(this.gameUrl, game, httpOptions);
+  }
+
+  getGamesByUserId(id: number): Observable<Game[]> {
+    return this.http.get<Game[]>(this.gameUrl + "/" + id, httpOptions);
+  }
+
+  updateGame(game: Game, id: number): Observable<Game> {
+    return this.http.put<Game>(this.gameUrl + "/" + id, game, httpOptions);
+  }
+}
