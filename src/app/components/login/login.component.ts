6708ff39fb02d89c8122dca66cb16f01af1aac92
Kevin Tran Huu <kevin.tranhuu@gmail.com>
2019-04-02 09:08:23 -0400

    Implemented login check/dashboard redirect

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -29,6 +29,9 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    if(this.myStorage.getItem("userId") != null && this.myStorage.getItem("userId") != ""){
+      this.router.navigate(['./dashboard']);
+    }
   }
 
 
